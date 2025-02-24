46f14aee18688677e07c4290f51082d877aa32df
Sam-1351 <shannon.brown0526@yahoo.com>
2025-02-24 18:36:38 +0000

    did lessons 1 and 2

--- lessons/00_Turtles/02_Meet_Tina.py
+++ lessons/00_Turtles/02_Meet_Tina.py
@@ -10,43 +10,43 @@ window.
 
 """
 
-import turtle                           # Tell Python we want to work with the turtle
+import turtle                        # Tell Python we want to work with the turtle
 turtle.setup(width=600, height=600)     # Set the size of the window
 
-tina = turtle.Turtle()                  # Create a turtle named tina
+bob =turtle.Turtle()                  # Create a turtle named tina
 
-tina.shape('turtle')                    # Set the shape of the turtle to a turtle
-tina.speed(2)                           # Make the turtle move as fast, but not too fast.
+bob.shape('turtle')                    # Set the shape of the turtle to a turtle
+bob.speed(1)                           # Make the turtle move as fast, but not too fast.
 
-tina.pencolor('blue')                   # Set the pen color to blue
-tina.forward(150)                       # Move tina forward by the forward distance
-tina.left(90)                           # Turn tina left by the left turn
+bob.pencolor('gold')                   # Set the pen color to blue
+bob.forward(150)                       # Move tina forward by the forward distance
+bob.left(90)                           # Turn tina left by the left turn
 
-tina.pencolor('red')                    # Set the pen color to red
-tina.forward(150)                       # Continuie the last two steps three more times
-tina.left(90)                           # to draw a square
+bob.pencolor('yellow')                    # Set the pen color to red
+bob.forward(150)                       # Continuie the last two steps three more times
+bob.left(90)                           # to draw a square
 
-tina.pencolor('green')                  # Set the pen color to green
-tina.forward(150)
-tina.left(90)
+bob.pencolor('dark cyan')                  # Set the pen color to green
+bob.forward(150)
+bob.left(90)
 
-tina.pencolor('purple')                 # Set the pen color to purple
-tina.forward(150)
-tina.left(90)
+bob.pencolor('teal')                 # Set the pen color to purple
+bob.forward(150)
+bob.left(90)
 
-tina.penup()                            # Lift the pen up so we can move tina without drawing
-tina.forward(20)                        # Move tina forward by 20
-tina.left(90)                           # Turn tina left by 90 degrees
-tina.forward(20)                        # Move tina forward by 20
-tina.write("Why, hello there!")         # Write the message "Why, hello there!"
-tina.backward(20)                       # Move tina backward by 20
+bob.penup()                            # Lift the pen up so we can move tina without drawing
+bob.forward(20)                        # Move tina forward by 20
+bob.left(90)                           # Turn tina left by 90 degrees
+bob.forward(20)                        # Move tina forward by 20
+bob.write("I'm bob,not tina!")         # Write the message "Why, hello there!"
+bob.backward(20)                       # Move tina backward by 20
 
-tina.goto(-50,0)
-tina.pendown()
-tina.color('red')                       # Set the color of tina to red
-tina.begin_fill()
-tina.circle(101, steps=50)
-tina.end_fill()
+bob.goto(-50,0)
+bob.pendown()
+bob.color('white')                       # Set the color of tina to red
+bob.begin_fill()
+bob.circle(101, steps=50)
+bob.end_fill()
 
 turtle.exitonclick()                    # Close the window when we click on it
 
